6fd462c451af7d7592f0c50a0ce0c0a193c67717
Sagar <sagarpatel9410@gmail.com>
2017-02-07 10:27:09 +0000

    added IG calculator

--- calculate_information_gain.m
+++ calculate_information_gain.m
@@ -1,4 +1,4 @@
-function ig = calculate_information_gain(k,idx,topics)
+function ig = calculate_information_gain(k,idx, user_data_set)
 %returns a column vector of information gain for each of the topics 
 
 %---------------------------------------------------
@@ -18,9 +18,49 @@ for i = 1:k
 end
 
 %---------------------------------------------------
+[topics,num_user] = size(user_data_set);
 
 %determine C^r_at - dist of users into classes that rated item a_t with r
 quantise = 0.05;
-C_r_at = zeros(k,topics,1/quantise);
+C_r_at = zeros(1/quantise,k,topics);
 
+for topic = 1:topics
+     for user = 1:num_user
+        quantised_rating = floor((user_data_set(topic,user)/quantise) + 1);
+        if(quantised_rating == (1/quantise) + 1)
+            quantised_rating = (user_data_set(topic,user)/quantise);
+        end
+        user_cluster_number = idx(user,1);
+        C_r_at(quantised_rating, user_cluster_number,topic) = C_r_at(quantised_rating, user_cluster_number,topic) + 1;
+     end
+end
 
+C_r_at = C_r_at/num_user;
+
+%determine H(C^r_at) 
+
+H_C_r_at = zeros(1/quantise,topics);
+
+for topic = 1:topics
+    for rating_quant = 1:(1/quantise)
+        for cluster = 1:k
+            if C_r_at(rating_quant,cluster,topic) ~= 0
+                H_C_r_at(rating_quant,topic) = H_C_r_at(rating_quant,topic) - (C_r_at(rating_quant,cluster,topic) * log2(C_r_at(rating_quant,cluster,topic)));                
+            end
+        end
+    end
+end
+
+%IG
+IG = zeros(topics,2);
+
+for topic = 1:topics
+    temp = 0;
+    for rating_quant = 1:(1/quantise)
+       temp = temp + ((norm(C_r_at(rating_quant,:,topic)))/norm(C))*H_C_r_at(rating_quant,topic);
+    end
+    IG(topic,1) = H_C - temp;
+    IG(topic,2) = topic;
+end
+
+ig = sortrows(IG,-1);
