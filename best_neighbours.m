f2415bb84631b5f61adbff34e5f32eea49d889bf
Sagar <sagarpatel9410@gmail.com>
2017-02-15 22:26:57 +0000

    rm

--- best_neighbours.m
+++
@@ -1,9 +0,0 @@
-function neighbours = best_neighbours(centroids, new_user_profile,l)
-[val,~] = size(centroids);
-neighbours = zeros(val,2);
-for k = 1:val
-    neighbours(k,1) = corr(new_user_profile(:,1), centroids(k,:)');
-    neighbours(k,2) = k;
-end
-
-neighbours = sortrows(neighbours,-1);
