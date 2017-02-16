b45499993ab5d961b370be4b1da801907e9a08a9
sagarpatel9410 <sp3613@eews506a-048.ic.ac.uk>
2017-02-16 19:00:34 +0000

    svd

--- Matlab_Implementation/SVD/dummy.m
+++ Matlab_Implementation/SVD/dummy.m
@@ -1,6 +1,28 @@
 clear
 rng('default');
-data = generating_data_set(50,35,0,30,0.1);
+data = generating_data_set(10,35,0.1,20,0.1);
 
-[U,S,V] = svd(data);
+[r,c] = size(data);
+
+for i = 1:r
+    avg = mean(data(i,:));
+    for j = 1:c
+        
+        if(data(i,j) == 0)
+            data(i,j) = avg;
+        end
+        
+    end
+end
+
+
+    
+%using SVD to predict uknown ratings of the entire Rating matrix
+
+[U,S,V] = svd(data');
+
+
+
+
+
 
