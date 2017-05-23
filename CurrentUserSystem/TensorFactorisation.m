72cb0f26682ff99e6807214bef0dace23f18152d
Sagar <sagarpatel9410@gmail.com>
2017-05-23 23:50:52 +0100

    ready to tensor factorisation

--- CurrentUserSystem/TensorFactorisation.m
+++ CurrentUserSystem/TensorFactorisation.m
@@ -1,7 +1,16 @@
-parfor i = 1:size(abc,1)
-   
-    idx = find(ismember(dataset(:,[1,2,3]), abc(i,:) ,'rows'));
-    a(i) = floor(mean(dataset(idx,4)));   
-    i
-end
-
+clear
+load algebra_dataset.mat
+% 
+% users = length(unique(dataset(:,1)));
+% questions = length(unique(dataset(:,2)));
+% mxtime = max(dataset(:,3));
+% 
+% utility = zeros(users,questions,mxtime);
+% attempts = zeros(users,questions,mxtime);
+% 
+% for i = 1:size(dataset,1)
+%     
+%     utility(dataset(i,1), dataset(i,2), dataset(i,3)) = dataset(i,4);
+%     attempts(dataset(i,1), dataset(i,2), dataset(i,3)) = 1;
+% 
+% end
